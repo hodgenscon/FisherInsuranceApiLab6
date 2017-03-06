67d055e75f167bd64c87f2f9abd22ef4f46f8a94
Connor Hodgens <Connor Hodgens>
2017-03-06 18:20:12 -0500

    refining

---
+++ wwwroot/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core"; 
+
+
+@Component({     
+        
+        selector: "fisher-insurance",
+        
+        template: '<home-page>'
+    
+}) 
+
+export class AppComponent { }
--- wwwroot/app/app.module.ts
+++ wwwroot/app/app.module.ts
@@ -3,9 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http'; 
 import 'rxjs/Rx'; // import our application component 
 import { AppComponent } from './app.component'; 
+import  { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/navbar/navbar.component';
+
 @NgModule({     
     declarations: [         
-        AppComponent     
+        AppComponent,
+        HomeComponent,
+        NavBarComponent     
         ],     
         imports: [         
             BrowserModule,         
