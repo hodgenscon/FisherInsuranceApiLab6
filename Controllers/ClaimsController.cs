5162801ba6dcd829c1929421bf07921c8b201a41
Connor Hodgens <Connor Hodgens>
2017-03-06 22:34:40 -0500

    Exercise 2 lab 6

--- Controllers/ClaimsController.cs
+++ Controllers/ClaimsController.cs
@@ -3,14 +3,14 @@ using FisherInsuranceApi.Models;
 using FisherInsuranceApi.Data;
     public class ClaimsController : Controller
     {
-
+        private readonly FisherContext db;
         public ClaimsController()
         {
-            
+            db = context;
         }
         public IActionResult GetClaims()
         {
-            return Ok();
+            return Ok(db.Claims);
         }
         public IActionResult Index()
         {
@@ -21,29 +21,45 @@ using FisherInsuranceApi.Data;
         public IActionResult Post([FromBody]Claim claim)
 
         {
+            var newClaim = db.Claims.Add(claim);
+            db.SaveChanges();
 
-            return Ok();
+            return CreatedAtRoute("GetClaim", new {id=claim.Id}, claim);
 
         }
 
     // GET api/claims/claim/5
-        [HttpGetAttribute("{id}")]
+        [HttpGetAttribute("{id}", Name = "GetClaim")]
 
         public IActionResult Get(int id)
         {
-            return Ok();
+            return Ok(db.Claims.Find(id));
         }
     // PUT api/claims/claim/id
         [HttpPutAttribute("{id}")]
-        public IActionResult Put(int id, [FromBody]string value)
+        public IActionResult Put(int id, [FromBody]Claim claim)
         {
-            return NoContent();
+            var newClaim = db.Claims.Find(id);
+            if (newClaim==null)
+            {
+                return NotFound();
+            }
+            newClaim = claim;
+            db.SaveChanges();
+
+            return Ok(newClaim);
         }
     // DELETE api/claims/claim/id
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            
-            return Ok();
+            var claimToDelete=db.Claims.Find(id);
+            if (claimToDelete==null){
+                return NotFound();
+            }
+            db.Claims.Remove(claimToDelete);
+            db.SaveChangesAsync();
+
+            return NoContent();
         }
     }
